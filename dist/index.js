3cadf3800b05ed633ca82631a8530343f8c3b2ee
ljp-projects <131211966+ljp-projects@users.noreply.github.com>
2023-11-18 06:29:42 +1100

    Changed Stuff
    Please note that this was commited via a Makefile

--- dist/index.js
+++ dist/index.js
@@ -9,9 +9,7 @@ const Player = {
      */
     Lines: {
         created: new Date().getTime(),
-        value: [
-            ["", 2]
-        ]
+        value: []
     }
 };
 const Game = {
@@ -59,13 +57,21 @@ addLineElement === null || addLineElement === void 0 ? void 0 : addLineElement.a
     if (Game.Money.value >= 100) {
         const name = prompt("What shall the name of the line be?") || "BUS LINE NAME";
         const id = new Date().getTime() || 0;
-        const generatedLine = [name, id];
+        let passengers = ~~(Math.random() * 10);
+        const generatedLine = [name, [id, passengers]];
         const generatedHTMLLine = `
         <li style="list-style: none;" id="BUS-LINE-${id}">
             <b id="BUS-LINE-${id}-NAME">${name}</b>
             <p id="BUS-LINE-${id}-ID">${id}</p>
+            <p>Per second: $<span id="BUS-LINE-${id}-PROFIT">${passengers * 0.5 + Player.Lines.value.length}</span></p>
         </li>
         `;
+        const earn = setInterval(() => {
+            if (Game.Money.add)
+                Game.Money.add(passengers * 0.5 + Player.Lines.value.length);
+            passengers = ~~(Math.random() * 10);
+            find(`#BUS-LINE-${id}-PROFIT`).textContent = `${passengers * 0.5 + Player.Lines.value.length}`;
+        }, 1000);
         Player.Lines.value.push(generatedLine);
         linesElement.innerHTML += generatedHTMLLine;
         if (Game.Money.take)
--- src/index.ts
+++ src/index.ts
@@ -22,10 +22,8 @@ const Player = {
      */
     Lines: {
         created: new Date().getTime(),
-        value: [
-            ["", 2]
-        ]
-    } as ImmutableStat<[string, number][]>
+        value: []
+    } as ImmutableStat<[string, [number, number]][]>
 }
 
 const Game = {
@@ -80,13 +78,21 @@ addLineElement?.addEventListener("click", () => {
     if (Game.Money.value >= 100) {
         const name: string = prompt("What shall the name of the line be?") || "BUS LINE NAME"
         const id: number = new Date().getTime() || 0
-        const generatedLine: [string, number] = [name, id]
+        let passengers = ~~(Math.random() * 10)
+        const generatedLine: [string, [number, number]] = [name, [id, passengers]]
         const generatedHTMLLine = `
         <li style="list-style: none;" id="BUS-LINE-${id}">
             <b id="BUS-LINE-${id}-NAME">${name}</b>
             <p id="BUS-LINE-${id}-ID">${id}</p>
+            <p>Per second: $<span id="BUS-LINE-${id}-PROFIT">${passengers * 0.5 + Player.Lines.value.length}</span></p>
         </li>
         `
+
+        const earn = setInterval(() => {
+            if (Game.Money.add) Game.Money.add(passengers * 0.5 + Player.Lines.value.length)
+            passengers = ~~(Math.random() * 10)
+            find<HTMLElement> (`#BUS-LINE-${id}-PROFIT`).textContent = `${passengers * 0.5 + Player.Lines.value.length}`
+        }, 1000)
 
         Player.Lines.value.push(generatedLine)
         linesElement.innerHTML  += generatedHTMLLine
